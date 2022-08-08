062ac96c6c25db7df852dd0b5285af6715c885c1
Nathan Vitor <nathanvitor38@gmail.com>
2022-08-08 16:04:31 -0300

    req 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,17 +1,33 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   state = { }
 
   render() {
+    const { assertions } = this.props;
+    const assertionsNumber = 3;
     return (
       <div>
-        <div data-testid="feedback-text">Feedback</div>
         <Header />
+        {
+          assertions < assertionsNumber
+            ? <p data-testid="feedback-text">Could be better...</p>
+            : <p data-testid="feedback-text">Well Done!</p>
+        }
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (store) => ({
+  assertions: store.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
